08a9fe58f1891d13054e60e8d656d31419b0e6e8
Rachel Diaz <Rachel7113@gmail.com>
2021-02-26 13:22:54 -0500

    added mod exports & require to each class to fix undefined employee error

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ const inquirer = require('inquirer');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const path = require("path");
 const fs = require("fs");
+const { ModernFakeTimers } = require('@jest/fake-timers');
+const { Module } = require('module');
+
+
+
+
+
+
+
+
+
+
+
+
