e06de71c8e97f8333d799a2986cf78325753ff16
Rachel Diaz <Rachel7113@gmail.com>
2021-03-06 17:21:22 -0500

    added prompts for all team members & jest to package.json

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const inquirer = require('inquirer');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
-const path = require("path");
 const fs = require("fs");
-const { ModernFakeTimers } = require('@jest/fake-timers');
-const { Module } = require('module');
-const templateGenerator = require('./src/template');
+
+const template = require('./src/template');
+let team = [];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+};
+
+function menuQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'menu',
+                message: 'Would you like to add another member or build your team?',
+                choices: ['Add another member', 'Build team']
+            }
+        ]).then(data => {
+            switch (data.menu) {
+                case 'Add another member':
+                    teamQuestions();
+                    break;
+                case 'Build team':
+                    console.log(team);
+                    writeToFile('./dist/Generated-Team.html', template(team));
+            }
+        })
+}
 
 function init() {
     inquirer
@@ -32,17 +59,91 @@ function init() {
                 type: 'input',
                 name: 'officeNumber',
                 message: 'What is the managers office number?',
-            },
+            }
         ]).then(data => {
-            console.log(data)
-            let manager = new Manager(data.name, data.id, data.email, data.officeNumber);
-            console.log(manager)
-            let template = templateGenerator(manager)
-            fs.writeFile('./dist/Generated-Team.html', template, function (err) {
-                if (err) throw err;
-                console.log('saved!');
-            })
+            let manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+            console.log(manager);
+            team.push(manager);
+            menuQuestions();
         })
+}
+
+teamQuestions = async () => {
+    try {
+        let choice = await inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'Please select your team members role below.',
+                    choices: ['Engineer', 'Intern']
+                }
+            ])
+        console.log(choice)
+        if (choice.role === 'Engineer') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is the team members name?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'What is the team members employee ID?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'What is the team members email address?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: 'Enter engineers github account',
+
+                    }
+                ]).then(data => {
+                    let engineer = new Engineer(data.name, data.id, data.email, data.github)
+                    team.push(engineer);
+                    console.log(engineer);
+                    menuQuestions();
+                })
+        } else {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is the team members name?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'What is the team members employee ID?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'What is the team members email address?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: "Please enter interns school name"
+                    },
+
+                ]).then(data => {
+                    let intern = new Intern(data.name, data.id, data.email, data.school)
+                    team.push(intern);
+                    console.log(intern);
+                    menuQuestions();
+                })
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 init();
